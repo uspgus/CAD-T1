3804132c14eb20c50f42ddea829e5d1f91070d34
Gustavo Ferreira <gusferreira1203@usp.br>
2024-09-27 00:39:44 -0300

    initial file created

---
+++ main.c
@@ -0,0 +1,5 @@
+// Manages input, computation, and presentation of a convolution operation
+
+// Eduardo Souza Rocha, Gustavo Vieira Ferreira, Isadora Carolina Siebert, Murilo Valentim Zabott, Rogério Lopes Lube
+// 2nd Semester 2024
+// University of São Paulo, Institute of Mathematical and Computer Science
